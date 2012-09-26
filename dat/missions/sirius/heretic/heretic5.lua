2244a9bb3ca8bea91197f1cc0b8ee6fc0161b3bf
l0k1 <justi.n.icholson@gmail.com>
2012-09-25 19:12:01 -0700

    More fixins.

--- dat/missions/sirius/heretic/heretic5.lua
+++ dat/missions/sirius/heretic/heretic5.lua
@@ -18,12 +18,11 @@ bmsg[1] = [[You walk up to the two serious looking men, wondering why you are ev
 bmsg[2] = [[Thomorr guestures for you to sit down. "When the Sirius launched their attack on our assets and churches, we suffered greatly. They didn't even stop at the men. They imprisoned women, children, even the elderly. Any one resisting was shot. They even went as far as to attempt undercover operations on all of our locations they knew about outside of their space. They nearly wiped us out, but we survived. We have several major centers operating now, and things are looking up. We've been activily recruiting, not hiding in the dark, and we feel it is time to strike back against those oppresive tyrants."]]
 bmsg[3] = [[Mase Attes pulls out a binder, and opens it. "This is a rough idea of what we've had going on in our navy since the attack." He points out some figures and a couple graphs, and begins to explain. "The Nasin really had no true organized navy prior to the attack on The Wringer, as we had no reason. We had a roughly hewn, disorganized militia. After the attack, we decided we wanted revenge. We needed revenge. So our leadership decided that we needed a true navy, and began hiring pilots from amongst our ranks, as well as buying up any and all combat worthy vessels to equip those pilots with."]]
 bmsg[4] = [[Thomorr, at this point, coughs and interupts Attes' monologue. "Where you come in, %s, is that we need you. You are a decent pilot, and you already own a good vessel. Our navy... suffered... during the Sirius attack, and we are looking for a few good pilots to help our navy out. We can't guarantee you anything, but we can offer you prestige, honor, and a few credits. As well as a chance to take out some Sirius. We want you in the Nasin Navy. What do you say?]] --playername
-convo_choice = {
-   "I say yes!", --[[links to bmsgand 9]]
-   "How much pay are we talking?", --[[links to bmsg[6]]
-   "Give me some time to think about it.",--[[links to bmsg[7]]
-   "I don't want any part in this heresy!"--[[links to bmsg[8]]
-   }
+convo_choice = {}
+convo_choice[1] = "I say yes!"
+convo_choice[2] = "How much pay are we talking?"
+convo_choice[3] = "Give me some time to think about it."
+convo_choice[4] = "I don't want any part in this heresy!"
 bmsg[5] = [[Attes smiles broadly. "Good. Good. Well, you will be working closely with me, as I will be your immediate commander. I'm glad to have you on board." He shakes your hand enthusiastically. "I have a current wing under me of about 6 ships. You will be flying with us. The fleet is about 18 ships."]]
 bmsg[6] = [[Thomorr shakes his head and gives Attes an "I told you he was about the money" look. He turns to you. "We will pay, and promote you, based on your loyalty to us and your performance. It will be on a per-mission basis. Base pay is 10,000 credits from here on forward. Of course, this is only the base pay. You'll be making much more. This mission, we will pay you %d."]] --reward
 bmsg[7] = [[Thomorr and Attes both rise and shake your hand. Thomorr speaks, "Please, take your time. We will be hanging around here if you wish to come back."]]
